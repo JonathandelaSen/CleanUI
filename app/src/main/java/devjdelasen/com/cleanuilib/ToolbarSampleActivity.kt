b5fd63f62dd41de6d58632c55549a180663927d8
jdelasen <jonathandelasen@gmail.com>
2020-10-11 03:03:13 +0200

    Added screenshot. Added listeners to samples

--- app/src/main/java/devjdelasen/com/cleanuilib/ToolbarSampleActivity.kt
+++ app/src/main/java/devjdelasen/com/cleanuilib/ToolbarSampleActivity.kt
@@ -4,12 +4,18 @@ import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_toolbar_sample.*
 
 class ToolbarSampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_toolbar_sample)
         tintStatusBar(resources.getColor(R.color.app_white, null), false, this)
+
+        simpleToolbar.setLeftClickListener(View.OnClickListener { TODO("Not yet implemented") })
+        simpleToolbar.setRight1ClickListener(View.OnClickListener { TODO("Not yet implemented") })
+        simpleToolbar.setRight2ClickListener(View.OnClickListener { TODO("Not yet implemented") })
+        simpleToolbar.setRight3ClickListener(View.OnClickListener { TODO("Not yet implemented") })
     }
 
 
